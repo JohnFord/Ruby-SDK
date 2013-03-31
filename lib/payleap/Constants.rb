0e7138444e97d655a0e09759d5325fafb9bbc566
John Ford <jwford@gmail.com>
2013-03-31 10:58:59 -0400

    Sets correct server URL constants

--- lib/payleap/Constants.rb
+++ lib/payleap/Constants.rb
@@ -1,4 +1,4 @@
 module Payleap
-  UAT_SERVER_URL = "https://uat.payleap.com/"
-  PRODUCTION_SERVER_URL = "https://secure1.payleap.com/"
+  UAT_SERVER_URL = "https://uat.payleap.com/TransactServices.svc"
+  PRODUCTION_SERVER_URL = "https://secure1.payleap.com/TransactServices.svc"
 end
