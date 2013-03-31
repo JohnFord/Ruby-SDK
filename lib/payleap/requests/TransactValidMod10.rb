4320654a601d7cd7cdf68c8c9ae5fc6bf9e2dd5f
John Ford <jwford@gmail.com>
2013-03-31 13:31:54 -0400

    TransactValidMod10 correctly handles valid responses

--- lib/payleap/requests/TransactValidMod10.rb
+++ lib/payleap/requests/TransactValidMod10.rb
@@ -2,8 +2,8 @@ module Payleap
   module TransactValidMod10
     def ValidMod10(cardnumber)
       data = {:CardNumber => cardnumber}
-      response = Payleap::Request.get(:params => data)
-      response["boolean"]["__content__"] == 'true'
+      response = Payleap::Request.get(:call => 'validMod10', :params => data)
+      response["boolean"] == 'true'
     end
   end
 end
