86b73e654d578cf03a5c78d81718541c6f63524d
John Ford <jwford@gmail.com>
2013-04-02 12:05:20 -0400

    Adds Customer

---
+++ lib/customer.rb
@@ -0,0 +1,8 @@
+module Payleap
+  class Customer
+    attr_accessor :customer_key, :customer_id, :first_name, :last_name,
+      :last_name, :department, :street1, :street2, :street3, :city, :state,
+      :province, :zip, :country, :day_phone, :evening_phone, :fax, :email,
+      :mobile_phone, :status
+  end
+end
